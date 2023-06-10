d44ef4d92a2fa05a9eb4401f1d055928c31a1a7d
JaredGaastra <JGaastra2@Gmail.com>
2023-06-09 20:21:29 -0700

    face lift with some css and working db w/ WTForms involved

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 from model import db, Quotes
 from forms import QuoteForm
 
 
 
 app = Flask(__name__)
-
+db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///Customers'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "secret"
+app.app_context().push()
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
     """Show homepage."""
 
@@ -23,10 +24,10 @@ def estimate():
     """Show estimate form."""
     form = QuoteForm()
     if form.validate_on_submit():
-        project_name = request.form.get('project_name')
-        project_description = request.form.get('project_description')
-        budget = request.form.get('budget')
-        start_date = request.form.get('start_date')
+        project_name = form.project_name.data
+        project_description = form.project_description.data
+        budget = form.budget.data
+        start_date = form.start_date.data
         flash(f"Project {project_name} has been created!")
         return redirect(url_for('homepage'))
     else:
