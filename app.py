0f7f05e5f05aa953d2edf38f07eae1ae001d19e5
JaredGaastra <JGaastra2@Gmail.com>
2023-06-14 18:18:38 -0700

    updated file pathing

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ from forms import QuoteForm
 
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Customers'
+
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///Customers'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "secret"
